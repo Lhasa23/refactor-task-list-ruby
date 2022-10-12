21ff676a8e2b264951a2cbfa25491aceb7435cec
eleven <sydryy@gmail.com>
2022-10-12 02:22:35 -0700

    refactor

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -4,31 +4,24 @@ require_relative 'task'
 class Tasks
   def initialize
     @tasks = {}
+    @task_set = []
   end
 
-  def show
+  def show(tasks = @tasks)
     show_arr = []
-    @tasks.each do |project_name, project_tasks|
-      show_arr << project_tasks.reduce([project_name]) { |result, task| result << "  #{task.task_item_string}" }.join("\n")
+    tasks.each do |sub_name, sub_tasks|
+      show_arr << sub_tasks.reduce([sub_name]) { |result, task| result << "  #{task.task_item_string}" }.join("\n")
     end
     show_arr.join("\n\n")
   end
 
   def show_by_deadline
-    task_arr = []
-    @tasks.each do |project_name, project_tasks|
-      task_arr << project_tasks
-    end
     new_tasks_group = {}
-    task_arr.flatten.sort { |a, b| a.deadline <=> b.deadline }.each do |task|
+    @task_set.sort { |a, b| a.deadline <=> b.deadline }.each do |task|
       new_tasks_group[task.deadline] = (new_tasks_group[task.deadline] || []) << task
     end
 
-    show_arr = []
-    new_tasks_group.each do |deadline, tasks|
-      show_arr << tasks.reduce([deadline]) { |result, task| result << "  #{task.task_item_string}" }.join("\n")
-    end
-    show_arr.join("\n\n")
+    show(new_tasks_group)
   end
 
   def add_project!(name)
@@ -40,25 +33,25 @@ class Tasks
     if project_tasks.nil?
       raise 'Project tasks not existed'
     end
-    @tasks[project_name] << Task.new(next_id, description, false, project_name)
+
+    task = Task.new(next_id, description, false, project_name)
+    @tasks[project_name] << task
+    @task_set << task
   end
 
   def find_task_by_id(id_string)
     id = id_string.to_i
-    @tasks.map { |project_name, project_tasks|
-      project_tasks.find { |t| t.id == id }
-    }.reject(&:nil?).first
+    @task_set.select { |v| v.id == id }.first
   end
 
   def find_task_by_date(date = Date.today.strftime("%Y-%m-%d"))
-    @tasks.map do |project_name, project_tasks|
-      project_tasks.select { |t| t.deadline == date }
-    end.reject(&:nil?).flatten
+    @task_set.select { |t| t.deadline == date }
   end
 
   def delete_task_by_id(id_string)
     task = find_task_by_id(id_string)
     @tasks[task.project].delete task
+    @task_set.delete task
   end
 
   private
